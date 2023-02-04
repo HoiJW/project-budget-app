e209593163649b579788d276617004ca688b8147
Hoi Wong <jameshf420@gmail.com>
2023-02-03 20:03:05 -0500

    added progress bar css

--- src/Components/BudgetCards.js
+++ src/Components/BudgetCards.js
@@ -2,9 +2,13 @@ function BudgetCards( { name , amount , max}){
     return(
         <div className='budget-cards'>
             <div className="titles">
-            <h2>{name}</h2>
-            <h3>${amount} / <span className="max">${max}</span> </h3>
+                <h2>{name}</h2>
+                <h3>${amount} / <span className="max">${max}</span> </h3>
             </div>
+            <div className="progress-bar">
+            
+            </div>
+
         </div>
     )
 }
