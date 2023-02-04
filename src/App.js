e4380e5f84f4ac45263ae95502fb28f5c65b2d53
Hoi Wong <jameshf420@gmail.com>
2023-02-04 11:48:05 -0500

    figuring totals and expenese object

--- src/App.js
+++ src/App.js
@@ -12,14 +12,21 @@ function App() {
   
   // create a statful value thats bound to input
   const[userInput, setUserInput] = useState('');
+  const[title, setTitle] = useState('');
   const [val, setVal] = useState(0);
+  const[total, setTotal] = useState(0)
   //add event listener that fires everytune there is a change in our input
 
   const handleInput = (event) => {
     setUserInput(event.target.value);
     setVal((v) => (event.target.validity.valid ? event.target.value :v))
   }
+  const handleTitle =(event) => {
+    setTitle(event.target.value);
+  }
+
   
+
   const handleSubmit = (event) => {
     //get the info from userinput STATE
     event.preventDefault();
@@ -27,12 +34,12 @@ function App() {
     //send it off to firebase using push function
     const database = getDatabase(firebase);
     const dbRef = ref(database);
-    
-    push(dbRef, userInput);
-
+    const expense = {userInput:userInput, title:title};
+    push(dbRef, expense);
     setUserInput('');
-    
   }
+
+  
   
   const handleRemove = (budgetId) => {
     const database = getDatabase(firebase);
@@ -53,10 +60,13 @@ function App() {
       const data = reponse.val();
       // set Budget stat to reflect database info
       const newState = [];
-
       for (let key in data) {
+        console.log(data[key]['userInput']);
         newState.push(
-          {key:key, amount: data[key]}
+          {key:key, amount: parseInt(data[key]['userInput']),
+          title:data[key]['title']
+        }
+          
         );
         // 1. make an object {}
         // 2. build a property called "key" that has a value of the object's key
@@ -70,9 +80,13 @@ function App() {
         //   name: "By Crom!"
         //  }
       }
-      
+      let newTotal = 0;
+      newState.forEach((object) => {
+        newTotal = newTotal + object['amount']
+        console.log(newTotal);
+      })
       setBudget(newState);
-    
+      setTotal(newTotal);
     });
   
   
@@ -80,9 +94,9 @@ function App() {
   //current we have add expense ✔️
   //need to rescrict to numbers only✔️
   //need to make each expense into own div and can be remove "onClick"✔️
-  //need add title into expense
+  //need add title into expense✔️
   
-  //need add budget that reflect in Total amount
+  //need add budget that reflect in Total amount ?
   //expense cant be added if larger than Total
 
   //make a variable "sum" to sum up the current expenses
@@ -93,7 +107,7 @@ function App() {
 
   
   return(
-    <div className="titles">
+    <div className="main">
       <h1>My Budgets</h1>
       <form action="submit">
         <label htmlFor="newBudget"></label>
@@ -103,16 +117,22 @@ function App() {
         pattern='[0-9]*' 
         value={(userInput, val)}
         />
+        
+        <label htmlFor="newTitle"></label>
+        <input onChange={handleTitle} 
+        type="text" 
+        id="newTitle"
+        value={(title)}
+        />
         <button onClick={handleSubmit}>Add Expense</button>
       </form>
       
-      <div>
-
-        
+      <div className='contents'>
           {budget.map ( (budget) => {
               return (
                 <div className='budget-cards'>
                 <div key={budget.key}>
+                  <h3>{budget.title}</h3>
                   <p>${budget.amount}</p>
                   <button onClick={() => {handleRemove(budget.key)}}>
                   remove
@@ -124,7 +144,7 @@ function App() {
       })}
       </div>
 
-      <Total sums={150} max={1000}/>
+      <Total sums={total} max={1500}/>
       
       {/* <div className='budget-cards'>
             <div className="titles">
