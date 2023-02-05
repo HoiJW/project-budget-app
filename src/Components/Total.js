c6fcd2d5eb4cbac139ccf9ecd97330fe38d7ed84
Hoi Wong <jameshf420@gmail.com>
2023-02-05 10:47:01 -0500

    change total into dynamic

--- src/Components/Total.js
+++ src/Components/Total.js
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 function Total( {sums, max} ){
     
     //color change useState
-    // const [bgc, setBgc] = useState("green")
+    const [bgc, setBgc] = useState("total green")
+    useEffect(() => {
+        if (sums > max){setBgc("total red")}
+        else if(sums <= max*0.4){setBgc("total green")}
+        else if(sums <= max*0.75){setBgc("total yellow")}
+    },[sums,max])
     return(
         
-        <div className='total'>
+        <div className={bgc}>
            <div className="titles">
                 <h2>Total Expenses</h2>
+                <p className="smile">😊</p>
+                <p className="meh">🤨</p>
+                <p className="sad">😥</p>
+
                 <h3>${sums} / <span className="max">${max}</span></h3>
             </div>
         </div>
